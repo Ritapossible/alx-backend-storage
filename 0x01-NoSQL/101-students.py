bca57f265100c425b5bf79162d4ee4273f5e6263
Ritapossible <egwuaturita95@gmail.com>
2024-05-14 14:09:34 -0700

    python function

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Defines top_students """
+
+
+def top_students(mongo_collection):
+    """ Prints all student sorted by average score"""
+    average = mongo_collection.aggregate(
+            [{"$project": {"_id": 1, "name": 1,
+            [{"$project": {"_id": 1, "name": 1,
+                           "averageScore": {
+                               "$avg": {
+                                   "$avg": "$topics.score", },
+                               }, "topics": 1, }, },
+                {"$sort": {"averageScore": -1}, }, ])
+    return average
